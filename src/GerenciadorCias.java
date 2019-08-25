a35ef11af47568b80196e73b58b1744524c4dc75
Roberto <robertodermato@gmail.com>
2019-08-25 20:29:02 -0300

    Adicionei método que lista todas as companhias no Gerenciador de Cias

--- src/GerenciadorCias.java
+++ src/GerenciadorCias.java
@@ -27,6 +27,14 @@
             }
             return null;
         }
+
+        public ArrayList<CiaAerea> listarTodas(){
+            ArrayList<CiaAerea> aux = new ArrayList<CiaAerea>();
+            for (int pos=0; pos < empresas.size(); pos++){
+                aux.add(empresas.get(pos));
+            }
+            return aux;
+        }
     }
 
 
