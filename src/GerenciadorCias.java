7151172cde12e049ed5c8b2795a64c1492b39a16
robertodermato <54421540+robertodermato@users.noreply.github.com>
2019-09-07 17:30:32 -0300

    Update GerenciadorCias.java
    Aprendendo a usar o git

--- src/GerenciadorCias.java
+++ src/GerenciadorCias.java
@@ -28,21 +28,21 @@
             return null;
         }
 
-        public ArrayList<CiaAerea> listarTodas(){
-            ArrayList<CiaAerea> aux = new ArrayList<CiaAerea>();
-            for (int pos=0; pos < empresas.size(); pos++){
-                aux.add(empresas.get(pos));
-            }
-            return aux;
-        }
-        
         //public ArrayList<CiaAerea> listarTodas(){
-        //ArrayList<CiaAerea> aux = new ArrayList<CiaAerea>();
-        //    for (CiaAerea cia: empresas){
-        //    aux.add(cia);
+        //    ArrayList<CiaAerea> aux = new ArrayList<CiaAerea>();
+        //    for (int pos=0; pos < empresas.size(); pos++){
+        //        aux.add(empresas.get(pos));
         //    }
         //    return aux;
         //}
+        
+        public ArrayList<CiaAerea> listarTodas(){
+        ArrayList<CiaAerea> aux = new ArrayList<CiaAerea>();
+            for (CiaAerea cia: empresas){
+            aux.add(cia);
+            }
+            return aux;
+        }
 
         public ArrayList<CiaAerea> listarTodas(){
           return new ArrayList<>(empresas);
